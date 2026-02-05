0b39010656408922060760288d8bebc9ef9f097b
ChlorineDoe <cwahldassule@brandeis.edu>
2026-02-05 17:06:47 +0000

    added dependency for atg-api and created Engine and Gameflow classes

---
+++ src/main/java/edu/brandeis/cosi103a/groupb/engine/Gameflow.java
@@ -0,0 +1,25 @@
+package edu.brandeis.cosi103a.groupb.engine;
+import edu.brandeis.cosi.atg.engine.Engine;
+
+
+public class Gameflow {
+    
+
+    public void buyCard() {
+        
+    }
+
+    public void playAction() {
+        
+    }
+
+    public void discardCard() {
+        
+    }
+
+    public void endTurn() {
+        
+    }
+
+
+}

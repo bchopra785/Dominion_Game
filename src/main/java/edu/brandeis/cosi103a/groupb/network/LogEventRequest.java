cc5c19883b4de67f1d7a3ebf847a157e18b066bd
Mango-42 <157906628+Mango-42@users.noreply.github.com>
2026-03-15 21:24:33 +0000

    logging events framework for remote player

---
+++ src/main/java/edu/brandeis/cosi103a/groupb/network/LogEventRequest.java
@@ -0,0 +1,41 @@
+package edu.brandeis.cosi103a.groupb.network;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import edu.brandeis.cosi.atg.decisions.Decision;
+import edu.brandeis.cosi.atg.event.Event;
+import edu.brandeis.cosi.atg.state.GameState;
+
+public class LogEventRequest {
+
+    @JsonProperty("state")
+    private GameState state;
+
+    @JsonProperty("event")
+    private Event event;
+
+    @JsonProperty("player_uuid")
+    private String playerUuid;
+
+    // Getters and setters
+    public GameState getState() { return state; }
+    public void setState(GameState state) { this.state = state; }
+
+    public Event getEvent() { return event; }
+    public void setEvent(Event event) { this.event = event; }
+
+    public String getPlayerUuid() { return playerUuid; }
+    public void setPlayerUuid(String playerUuid) { this.playerUuid = playerUuid; }
+
+    @Override
+    public String toString() {
+        return "LogEventRequest{" +
+                "state=" + state +
+                ", event=" + event +
+                ", playerUuid='" + playerUuid + '\'' +
+                '}';
+    }
+}
